fe4c2d4b89bb93d71ac9eeaf6848b6856dd21a70
Fortuotu <ott.saluvere@gmail.com>
2026-02-09 09:31:37 +0200

    3. Komponendi sisustamine andmetega

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,29 @@ import './App.css'
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [
+    {
+      date : new Date(2026, 0, 10),
+      title : 'New website so cool to read',
+      price : 25.99
+    },
+    {
+      date : new Date(2026, 0, 5),
+      title : 'Ligma',
+      price : 99.99
+    },
+    {
+      date : new Date(2026, 0, 6),
+      title : 'Balls',
+      price : 12.59
+    },
+  ]
+
   return (
-    <div className='App'>
-      <ExpenseItem />
-      <ExpenseItem />
+    <div>
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
+      <ExpenseItem data={expenses[2]}/>
     </div>
   )
 }
